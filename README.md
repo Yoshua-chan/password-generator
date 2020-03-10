d70863f1b345f1b1e742abf01033e1c5914b49a8
Yoshua-chan <filipek.kacper311@gmail.com>
2020-03-10 14:52:52 +0100

    somewhat working and not looking terrible

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # password-generator
+
+the name speaks for itself, if you think that my code is bad and can be improved (which is probably the case) feel free to fix it
--- main.js
+++ main.js
@@ -1,1 +1,59 @@
-console.log('siema'); 
+const result = document.getElementById('result');
+const form = document.getElementById('myform');
+const lowerCheckbox = document.getElementById('lowerCheckbox');
+const upperCheckbox = document.getElementById('upperCheckbox');
+const numberCheckbox = document.getElementById('numberCheckbox');
+const symbolCheckbox = document.getElementById('symbolsCheckbox');
+const lenght = document.getElementById('lenght')
+let CHARSET = []
+lenght.addEventListener('click', (e) => {
+    e.preventDefault;
+    lenght.select();
+})
+form.addEventListener('submit', onSubmit);
+
+function onSubmit (event) {
+    event.preventDefault();
+    let password = 'chuj';
+    let lowerValue = lowerCheckbox.checked;
+    let upperValue = upperCheckbox.checked;
+    let numberValue = numberCheckbox.checked;
+    let symbolValue = symbolCheckbox.checked;
+    let lenghtValue = lenght.value;
+    if(lowerValue + upperValue + numberValue + symbolValue == 0){
+        console.log('You mast check at least one checkbox'); 
+    } else {
+        appendCharset(lowerValue, upperValue, numberValue, symbolValue);
+        password = generate(CHARSET, lenghtValue);
+        result.textContent = password;
+    }
+}
+function appendCharset(low, upp, num, sym) {
+    CHARSET = [];
+    if(low === true) {
+        CHARSET = CHARSET.concat(rowToArray(97, 122));
+    }
+    if(upp === true ) {
+        CHARSET = CHARSET.concat(rowToArray(65, 90));
+    }
+    if(num === true) {
+        CHARSET = CHARSET.concat(rowToArray(48, 57));
+    }
+    if(sym === true) {
+        CHARSET = CHARSET.concat(rowToArray(33, 47)).concat(rowToArray(58, 64)).concat(rowToArray(92, 95));
+    }
+}
+function rowToArray(first, last) {
+    const array = []
+    for(i = first; i<= last; i++) {
+        array.push(i);
+    }
+    return array;
+}
+function generate(set = [], size) {
+    password = '';
+    for(let i = 0; i<size; i++) {
+        password = password.concat(String.fromCharCode(set[Math.floor(Math.random() * set.length)]))
+    }
+     return password
+}
